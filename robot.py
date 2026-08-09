6f56501cc079089d130566aae5d69741e5821a02
Nnded <chuabernard0527@gmail.com>
2026-08-10 07:56:13 +0800

    refactor: switch battery attribute from mangled to protected

--- robot.py
+++ robot.py
@@ -11,25 +11,25 @@ class Robot(ABC):
 
     def __init__(self, name: str, battery: int = MAX_BATTERY_VALUE):
         self.name = name
-        self.__battery = battery
+        self._battery = battery # _single_leading_underscore to signal protected instance attribute and for internal use only
         self._population += 1
 
     @property
     def battery(self) -> int:
-        return self.__battery # double leading underscore to invoke name mangling
+        return self._battery # 
 
     @battery.setter
     def battery(self, charge: int) -> None:
         if not (self.MIN_BATTERY_VALUE <= charge <= self.MAX_BATTERY_VALUE):
             raise ValueError(f"Charge must be between {self.MIN_BATTERY_VALUE} and {self.MAX_BATTERY_VALUE}, but got {charge}")
 
-        self.__battery = charge
+        self._battery = charge
     
     def __str__(self):
-        return f"Robot: {self.name}, {self.__battery}%.__battery"
+        return f"Robot: {self.name}, {self._battery}%.__battery"
 
     def __repr__(self):
-        return f"Robot(name={self.name},.__battery={self.__battery}, manufacturer={self._manufacturer}, population={self._population})"
+        return f"Robot(name={self.name},.__battery={self._battery}, manufacturer={self._manufacturer}, population={self._population})"
 
     @abstractmethod
     def perform_task(self) -> None:
