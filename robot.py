3ffd845ebcbd7d43c17356a551e5e4fe8d380ae2
Nnded <chuabernard0527@gmail.com>
2026-08-10 15:13:50 +0800

    fix: increment class attribute instead of instance attribute

--- robot.py
+++ robot.py
@@ -12,7 +12,10 @@ class Robot(ABC):
     def __init__(self, name: str, battery: int = MAX_BATTERY_VALUE):
         self.name = name
         self._battery = battery # _single_leading_underscore to signal protected instance attribute and for internal use only
-        self._population += 1
+        Robot._population += 1
+
+    def __del__(self):
+        Robot._population -= 1
 
     @property
     def battery(self) -> int:
@@ -28,7 +31,7 @@ class Robot(ABC):
     @property 
     # setting population as property was not in the instructions but population should be read-only and NOT accessible outside this class to ensure it is the source of truth for the instance count.
     def population(self) -> int:
-        return self._population
+        return Robot._population
     
     def __str__(self):
         return f"Robot: {self.name}, {self._battery}%.__battery"
