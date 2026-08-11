5593a410b3427e05db55ce4468a002b3dcc0bc6e
Nnded <chuabernard0527@gmail.com>
2026-08-11 11:15:42 +0800

    tests: add tests for drone subclass

---
+++ tests/test_drone.py
@@ -0,0 +1,22 @@
+import unittest
+from drone import Drone
+from robot import Robot
+
+class DroneTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.drone = Drone(name="Reaper")
+
+    def test_super_init(self) -> None:
+        self.assertEqual(self.drone.name, "Reaper")
+
+    def test_battery_usage(self) -> None:
+        initial_battery_percentage = self.drone.battery
+        self.drone.perform_task()
+        self.assertNotEqual(initial_battery_percentage, self.drone.battery)
+
+    def test_insufficient_battery_usage(self) -> None:
+        with self.assertRaises(ValueError):
+            self.drone.use_battery(101)
+
+    def test_unique_attr(self) -> None:
+        self.assertTrue(hasattr(self.drone, "current_altitude_feet"))
