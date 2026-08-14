2b5d3b8e2aa7e043f6a9799f7c374a8015d567d0
Nnded <chuabernard0527@gmail.com>
2026-08-14 15:00:28 +0800

    feat: add insufficient battery exception handling

--- utils.py
+++ utils.py
@@ -1,5 +1,24 @@
 from robot import Robot
+from drone import Drone
+from exceptions import InsufficientBatteryError
+import logging
 
 def fleet_report(robots: list[Robot]) -> None:
     for robot in robots:
         print(str(robot))
+
+def run_task_safely(robot: Robot, **kwargs) -> None:
+    try:
+        output = robot.perform_task()
+    except InsufficientBatteryError as e:
+        logging.error(
+            f"{e.name} requires {e.required_battery}% battery for this task, but only has {e.available_battery}% battery left.",
+            exc_info=True, 
+            stack_info=True
+        )
+    else:
+        print(output)
+    finally:
+        print(
+            f"{robot.name} at {robot.battery}% battery."
+        )
